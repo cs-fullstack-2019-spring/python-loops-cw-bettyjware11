1bf1111353a369f1bae68974017c30bf05597f9c
student <bettyjcawthon@gmail.com>
2019-01-18 15:48:56 -0600

    loops afternoon cw

---
+++ loops-afternoon cw.py
@@ -0,0 +1,38 @@
+def main():
+    #problem1()
+    problem2()
+    #problem3()
+    #problem4()
+
+#Print -20 to and including 50. Use any loop you want.
+def problem1():
+    while count == -20:
+        if count <=50:
+            break
+            print("loop")
+
+#Create a loop that prints even numbers from 0 to and including 20.
+def problem2():
+    while count == 0:
+        if count <=20:
+            print
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    if __name__ == '__main__':
+        main()
